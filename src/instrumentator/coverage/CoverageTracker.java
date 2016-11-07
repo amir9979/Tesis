e334607adaa63a5f1c4e0115314426afc1fc75f6
jheredia <joaquinheredia93@gmail.com>
2016-11-07 06:05:40 -0300

    Added tracking of success and failures

--- src/instrumentator/coverage/CoverageTracker.java
+++ src/instrumentator/coverage/CoverageTracker.java
@@ -14,13 +14,18 @@ import java.util.Map;
 import javax.management.RuntimeErrorException;
 
 import instrumentator.utils.Pair;
-
+/*
+ * 
+ * This class contains the maps that track both the coverage and final stats of the tool
+ */
 public class CoverageTracker {
 
-	//Maps filenames and line numbers to true (Executed) or false (Not executed)
-	private static Map<String, Map<Integer, Boolean>> coverage = new HashMap<String, Map<Integer,Boolean>>();
+	//Maps line numbers to true (Executed) or false (Not executed)
+	private static Map<Integer, Boolean> coverage = new HashMap<Integer,Boolean>();
 
-	private static Map<String, Map<String,Pair<Integer, Integer>>> stats = new HashMap<String, Map<String,Pair<Integer, Integer>>>();
+	
+	//Maps lines numbers to amount of executions on passing and failing tests
+	private static Map<Integer, Pair<Integer, Integer>> stats = new HashMap<Integer, Pair<Integer, Integer>>();
 	
 	/**
 	 * Attempt to use Lcov records as used in: http://ismail.badawi.io/blog/2013/05/03/writing-a-code-coverage-tool/
@@ -41,28 +46,19 @@ public class CoverageTracker {
 				throw new RuntimeException(e);
 			}
 		}
+		System.out.println(reportPath);
 	}
 	
 	private static String generateReport(){
 		StringBuilder sb = new StringBuilder();
-		for (String filename: stats.keySet()){
-			sb.append("FILE: "+filename+"\n");
-			for (Map.Entry<String, Pair<Integer, Integer>> stat : stats.get(filename).entrySet()){
-				sb.append(String.format("LINE: %s passed: %i failed %i \n", stat.getKey(),stat.getValue().getLeft(),stat.getValue().getRight()));
-			}
-			sb.append("END OF REPORT \n");
-		}
+		sb.append(generateLcov());
 		return sb.toString();
 	}
 	
 	private static String generateLcov(){
 		StringBuilder sb = new StringBuilder();
-		for (String filename : coverage.keySet()){
-			sb.append("SF:"+filename+"\n");
-			for (Map.Entry<Integer, Boolean> line : coverage.get(filename).entrySet()){
-				sb.append(String.format("DA:%d,%d \n", line.getKey(), line.getValue() ? 1:0));
-			}
-			sb.append("end_of_record\n");
+		for (Integer line : stats.keySet()){
+			sb.append("LINE: "+line.toString()+" - "+stats.get(line).toString()+"\n");
 		}
 		return sb.toString();
 	}
@@ -96,23 +92,40 @@ public class CoverageTracker {
 	
 	/**
 	 * Mark a block as executed.
-	 * @param filename the name of the file.
 	 * @param line the number of the executed block first line.
 	 */
-	public static void markExecuted(String filename, int line) {
-		//If it's not already in the Map, add it.;s
-		if (!stats.containsKey(filename)) {
-			stats.put(filename, new Pair(0,0));
-		}
-		//Then set it as executed.
-		stats.get(filename).put(line, true);
+	public static void markExecuted(int line) {
+		//Mark the block as executed.
+		coverage.put(line, true);
 	}
 	
-	public static void markExecutable(String filename, int line) {
-		if (!coverage.containsKey(filename)){
-			coverage.put(filename, new HashMap<Integer,Boolean>());
+	
+	/**
+	 * Add count according to the value passed to the method. This method should be called on teardown in the test suite
+	 */
+	public static void markTestResult(boolean result){
+		//Search all the executed lines
+		for (Integer line : coverage.keySet()){
+			//If it doesn't exist on the stats map, add it.
+			if (!stats.containsKey(line)){
+				stats.put(line, new Pair<Integer, Integer>(0,0));
+			}
+			//Add the count afterwards, on the first one if it's true, on the second if it's false.
+			Pair<Integer,Integer> lineCount = stats.get(line);
+			if (result){
+				lineCount.setLeft(lineCount.getLeft()+1);
+			} else {
+				lineCount.setRight(lineCount.getRight()+1);
+			}
+			stats.put(line, lineCount);
 		}
-		coverage.get(filename).put(line, false);
+	}
+	
+	/**
+	 * Call this on every setUp for the test so a new coverage map will generate.
+	 */
+	public static void clearCoverageMap(){
+		coverage.clear();
 	}
 	/*
 	 * We add a Shutdown Hook, that is, a thread that will launch on the JVM shutdown sequence.
